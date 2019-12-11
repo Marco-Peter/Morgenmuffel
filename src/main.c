430ebdfaeb5c02ceb2eadd906c7fa82a6a0d6f2e
Marco Peter <marco@peter-net.ch>
2019-12-11 01:15:21 +0100

    Trying to get SPI flash working with nvs
    Signed-off-by: Marco Peter <marco@peter-net.ch>

--- src/main.c
+++ src/main.c
@@ -4,10 +4,74 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
 #include <sys/printk.h>
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/flash.h>
+#include <fs/nvs.h>
 
+#define FLASH_AREA_STORAGE_OFFSET 0;
+
+static int initFlash(void);
+
+static struct nvs_fs nvs;
+
+/**
+ * Application entry point
+ */
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+  int rc;
+
+  printk("Hello World! %s\n", CONFIG_BOARD);
+
+  rc = initFlash();
+  if (rc)
+  {
+    printk("Failed initialisation!\n");
+  }
+}
+
+/**
+ * Initialise all flash memory related functions, including nvs and file system.
+ *
+ * @return -ERRNO if something wrong happened.
+ */
+
+static int initFlash(void)
+{
+  struct flash_pages_info info;
+  struct device *dev = NULL;
+  int rc;
+
+  dev = device_get_binding(DT_SPI_1_NAME);
+  printk("SPI1 address: 0x%x\n", (uint32_t)dev);
+  dev = device_get_binding("m25p16");
+
+  nvs.offset = FLASH_AREA_STORAGE_OFFSET;
+  rc = flash_get_page_info_by_offs(
+      dev,
+      nvs.offset,
+      &info);
+
+  if (rc)
+  {
+    printk("Unable to get page info\n");
+    return rc;
+  }
+
+  printk("Sector size is %d bytes\n", info.size);
+  /*
+  nvs.sector_size = info.size;
+  nvs.sector_count = 3U;
+
+  rc = nvs_init(&nvs, DT_FLASH_DEV_NAME);
+  if (rc)
+  {
+    printk("Flash initialisation failed\n");
+    return rc;
+  }
+  */
+  printk("Finished, all good!\n");
+  return 0;
 }
---
+++ src/parameters.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Marco Peter
+ * 
+ * This file contains all available parameter identifiers on the system.
+ * Usage of the parameters are the task of the implementing modules.
+ */
+
+#pragma once
+
+#define PARAM_SYSTEM_VOLUME             0
+#define PARAM_SYSTERM_POWERSTATE        1
+
+#define PARAM_PLAYLIST_CURRENT_ENTRY    1000
+#define PARAM_PLAYLIST_START            1001
