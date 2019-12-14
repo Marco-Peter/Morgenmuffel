3c60e4a099a1bf2bb6139a958cc3230e3dc31eb7
Marco Peter <marco@peter-net.ch>
2019-12-14 21:02:49 +0100

    Flash memory and NVS is operational

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ static struct nvs_fs nvs;
  */
 void main(void)
 {
-  int rc;
+  int rc = 0;
 
   printk("Hello World! %s\n", CONFIG_BOARD);
 
@@ -43,49 +43,10 @@ void main(void)
 static int initFlash(void)
 {
   struct flash_pages_info info;
-  struct device *spi1;
-  struct device *spi2;
   struct device *spiflash;
   int rc;
 
-  struct spi_cs_control spiControl_flash =
-      {
-          .gpio_dev = device_get_binding(DT_INST_0_JEDEC_SPI_NOR_CS_GPIOS_CONTROLLER),
-          .gpio_pin = DT_INST_0_JEDEC_SPI_NOR_CS_GPIOS_PIN,
-          .delay = 0};
-
-  struct spi_config spiConfig_flash =
-      {
-          .frequency = 75000000,
-          .operation =
-              SPI_OP_MODE_MASTER |
-              SPI_TRANSFER_MSB |
-              SPI_WORD_SET(8) |
-              SPI_LINES_SINGLE,
-          .slave = 0,
-          .cs = &spiControl_flash};
-
-  uint8_t query = 0x9F;
-  uint8_t ans[3];
-  struct spi_buf buffers[] = {
-      {.buf = &query,
-       .len = sizeof(query)},
-      {.buf = &ans,
-       .len = sizeof(ans)}};
-
-  struct spi_buf_set bufSet =
-      {
-          .buffers = buffers,
-          .count = sizeof(buffers) / sizeof(buffers[0])};
-
-  spi1 = device_get_binding(DT_SPI_1_NAME);
-  printk("SPI1 address: 0x%x\n", (uint32_t)spi1);
-  spi2 = device_get_binding(DT_SPI_2_NAME);
-  printk("SPI2 address: 0x%x\n", (uint32_t)spi2);
-
-  rc = spi_transceive(spi2, &spiConfig_flash, &bufSet, &bufSet);
-
-  spiflash = device_get_binding(DT_INST_0_JEDEC_SPI_NOR_LABEL);
+  spiflash = device_get_binding("M25P16");
   printk("SPI FLASH address: 0x%x\n", (uint32_t)spiflash);
 
   nvs.offset = FLASH_AREA_STORAGE_OFFSET;
@@ -100,18 +61,16 @@ static int initFlash(void)
     return rc;
   }
 
-  printk("Sector size is %d bytes\n", info.size);
-  /*
   nvs.sector_size = info.size;
   nvs.sector_count = 3U;
 
-  rc = nvs_init(&nvs, DT_FLASH_DEV_NAME);
+  rc = nvs_init(&nvs, "M25P16");
   if (rc)
   {
     printk("Flash initialisation failed\n");
     return rc;
   }
-  */
+
   printk("Finished, all good!\n");
   return 0;
 }
