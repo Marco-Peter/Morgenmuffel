1dce18037b4cec7e52771cdbf83c73618336d586
Marco Peter <marco@peter-net.ch>
2020-01-14 20:44:14 +0100

    Getting file system properties

--- src/storage.c
+++ src/storage.c
@@ -36,15 +36,24 @@ static struct fs_mount_t flash_storage_mnt = {
  *****************************************************************************/
 int storage_init(void) {
   int rc;
-  unsigned int id = (uintptr_t)flash_storage_mnt.storage_dev;
+  struct fs_mount_t *mp = &flash_storage_mnt;
+  unsigned int id = (uintptr_t)mp->storage_dev;
   const struct flash_area *pfa;
+  struct fs_statvfs sbuf;
 
   rc = flash_area_open(id, &pfa);
   checkRc();
 
-  rc = fs_mount(&flash_storage_mnt);
+  rc = fs_mount(mp);
   checkRc();
 
+  rc = fs_statvfs(mp->mnt_point, &sbuf);
+  checkRc();
+
+  printk("%s: bsize = %lu; frsize = %lu;"
+         " blocks = %lu; bfree = %lu\n",
+         mp->mnt_point, sbuf.f_bsize, sbuf.f_frsize, sbuf.f_blocks,
+         sbuf.f_bfree);
   return 0;
 }
 
