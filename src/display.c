009643273a79ae158af0a6c0e79ddd5361810d61
Marco Peter <marco@peter-net.ch>
2020-10-12 18:28:46 +0200

    Adding command queue to display handler

--- src/display.c
+++ src/display.c
@@ -13,6 +13,8 @@ LOG_MODULE_REGISTER(disp);
 #define DISPLAY_THREAD_PRIO 2
 #define DISPLAY_THREAD_STACK 256
 
+K_FIFO_DEFINE(display_fifo);
+
 static const struct device *display;
 static const struct device *power_on;
 
@@ -24,6 +26,11 @@ int display_off(void)
 	return retval;
 }
 
+void display_command(display_command_t cmd)
+{
+	k_fifo_put(&display_fifo, cmd);
+}
+
 static void display_func(void)
 {
 	int retval;
@@ -31,23 +38,29 @@ static void display_func(void)
 	display = device_get_binding("DISPLAY");
 	if (display == NULL) {
 		LOG_ERR("Display device not found");
-                return;
+		return;
 	}
 	power_on = device_get_binding("GPIOD");
 	if (power_on == NULL) {
 		LOG_ERR("Power supply pin not found");
-                return;
+		return;
 	}
 	retval = gpio_pin_set(power_on, POWER_ON_PIN, 1);
 	if (retval != 0) {
 		LOG_ERR("Failed to set power supply pin");
-                return;
+		return;
 	}
 
-        for(;;) {
-                k_sleep(K_MSEC(10));
-                lv_task_handler();
-        }
+	for (;;) {
+		display_command_t cmd;
+
+		k_sleep(K_MSEC(10));
+		while ((cmd = (display_command_t)k_fifo_get(
+				&display_fifo, K_NO_WAIT)) != NULL) {
+			cmd();
+		}
+		lv_task_handler();
+	}
 }
 
 K_THREAD_DEFINE(display_thread, DISPLAY_THREAD_STACK, display_func, NULL, NULL,
--- src/display.h
+++ src/display.h
@@ -1,6 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+typedef void (*display_command_t)(void);
+
 int display_init(void);
+int display_off(void);
+void display_command(display_command_t cmd);
 
 #endif /* DISPLAY_H */
