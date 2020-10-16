8bbcff48fd5a46d127fa2f0bacc3f117ff922144
Marco Peter <marco@peter-net.ch>
2020-10-17 00:55:47 +0200

    Completed MS5637 implementation. Added barometer data to main application

--- drivers/ms5637/zephyr/ms5637.c
+++ drivers/ms5637/zephyr/ms5637.c
@@ -10,9 +10,10 @@
 #include <drivers/sensor.h>
 #include <logging/log.h>
 
-LOG_MODULE_REGISTER(MS5637, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(MS5637, LOG_LEVEL_DBG);
 
 #define MS5637_CMD_RESET 0x1E
+
 #define MS5637_CMD_CONV_P_256 0x40
 #define MS5637_CMD_CONV_P_512 0x42
 #define MS5637_CMD_CONV_P_1024 0x44
@@ -39,12 +40,12 @@ LOG_MODULE_REGISTER(MS5637, LOG_LEVEL_INF);
 #define MS5637_CMD_READ_C5 0xAA
 #define MS5637_CMD_READ_C6 0xAC
 
-#define MS5637_CONV_TIMER_OSR8192_MS 17
-#define MS5637_CONV_TIMER_OSR4096_MS 9
-#define MS5637_CONV_TIMER_OSR2048_MS 5
-#define MS5637_CONV_TIMER_OSR1024_MS 3
-#define MS5637_CONV_TIMER_OSR512_MS 2
-#define MS5637_CONV_TIMER_OSR256_MS 1
+#define MS5637_CONV_TIMER_OSR8192_MS 20
+#define MS5637_CONV_TIMER_OSR4096_MS 12
+#define MS5637_CONV_TIMER_OSR2048_MS 7
+#define MS5637_CONV_TIMER_OSR1024_MS 5
+#define MS5637_CONV_TIMER_OSR512_MS 3
+#define MS5637_CONV_TIMER_OSR256_MS 2
 
 struct ms5637_config {
 	char *i2c_bus_label;
@@ -83,12 +84,14 @@ static inline const uint16_t i2c_address(const struct device *dev)
 
 static inline int send_command(const struct device *dev, uint8_t cmd)
 {
+	LOG_DBG("Send command 0x%X to address 0x%X", cmd, i2c_address(dev));
 	return i2c_write(i2c_device(dev), &cmd, sizeof(cmd), i2c_address(dev));
 }
 
 static inline int read_data(const struct device *dev, uint8_t *data,
 			    uint32_t len)
 {
+	LOG_DBG("Read %d bytes from address 0x%X", len, i2c_address(dev));
 	return i2c_read(i2c_device(dev), data, len, i2c_address(dev));
 }
 
@@ -184,6 +187,12 @@ static int init(const struct device *dev)
 	data->temp_sens = prom[12];
 	data->temp_sens <<= 8;
 	data->temp_sens |= prom[13];
+	LOG_INF("%s: sens_t1 = %u", dev->name, data->sens_t1);
+	LOG_INF("%s: off_t1 = %u", dev->name, data->off_t1);
+	LOG_INF("%s: tcs = %u", dev->name, data->tcs);
+	LOG_INF("%s: tco = %u", dev->name, data->tco);
+	LOG_INF("%s: t_ref = %u", dev->name, data->t_ref);
+	LOG_INF("%s: temp_sens = %u", dev->name, data->temp_sens);
 	return rc;
 }
 
@@ -200,17 +209,19 @@ static int convert(const struct device *dev, uint8_t cmd, uint8_t wait,
 		return rc;
 	}
 	k_sleep(K_MSEC(wait));
-	rc = read_data(dev, rx_data, sizeof(rx_data));
+	rc = send_command(dev, MS5637_CMD_CONV_READ_ADC);
 	if (rc != 0) {
-		LOG_ERR("%s: reading measurement data failed with error % d",
+		LOG_ERR("%s: requesting adc data failed with error %d",
 			dev->name, rc);
 		return rc;
 	}
-	*value = rx_data[0];
-	*value <<= 8;
-	*value |= rx_data[1];
-	*value <<= 8;
-	*value |= rx_data[2];
+	rc = read_data(dev, rx_data, sizeof(rx_data));
+	if (rc != 0) {
+		LOG_ERR("%s: reading measurement data failed with error %d",
+			dev->name, rc);
+		return rc;
+	}
+	*value = (rx_data[0] << 16) | (rx_data[1] << 8) | rx_data[2];
 	return 0;
 }
 
@@ -222,9 +233,11 @@ static int32_t calc_pressure(const struct device *dev, uint32_t value)
 	int64_t sens;
 	int32_t press;
 
-	offs = (data->off_t1 << 17) + ((data->tco * data->dt) >> 6);
-	sens = (data->sens_t1 << 16) + ((data->tcs * data->dt) >> 7);
-	press = (((value * sens) >> 21) - offs) >> 15;
+	offs = ((int64_t)data->off_t1 << 17) +
+	       (((int64_t)data->tco * (int64_t)data->dt) >> 6);
+	sens = ((int64_t)data->sens_t1 << 16) +
+	       (((int64_t)data->tcs * (int64_t)data->dt) >> 7);
+	press = ((((int64_t)value * sens) >> 21) - offs) >> 15;
 	return press;
 }
 
@@ -238,11 +251,15 @@ static int sample_fetch(const struct device *dev, enum sensor_channel chan)
 		rc = convert(dev, data->conv_cmd_temp, data->conv_time_temp,
 			     &value);
 		if (rc == 0) {
-			data->dt = ((int32_t)value - data->t_ref) << 8;
+			data->dt = (int32_t)value - (data->t_ref << 8);
+			LOG_DBG("%s: value = %d, dt = %d", dev->name, value,
+				data->dt);
 			rc = convert(dev, data->conv_cmd_press,
 				     data->conv_time_press, &value);
 			if (rc == 0) {
 				data->pressure = calc_pressure(dev, value);
+				LOG_DBG("%s: value = %d, press = %d", dev->name,
+					value, data->pressure);
 			}
 		}
 	} else if (chan == SENSOR_CHAN_AMBIENT_TEMP) {
@@ -264,12 +281,26 @@ static int channel_get(const struct device *dev, enum sensor_channel chan,
 	int rc = 0;
 
 	if (chan == SENSOR_CHAN_AMBIENT_TEMP) {
-		int32_t temp = 2000 + ((data->dt * data->temp_sens) >> 23);
+		uint16_t temp_sens;
+		int32_t dt;
+		uint8_t div;
+
+		if (data->dt > UINT16_MAX) {
+			dt = data->dt >> 1;
+			temp_sens = data->temp_sens >> 1;
+			div = 21;
+		} else {
+			dt = data->dt;
+			temp_sens = data->temp_sens;
+			div = 23;
+		}
+		int32_t temp = 2000 + ((dt * temp_sens) >> div);
+		LOG_DBG("%s: temp = %d", dev->name, temp);
 		val->val1 = temp / 100;
 		val->val2 = (temp % 100) * 10000;
 	} else if (chan == SENSOR_CHAN_PRESS) {
 		val->val1 = data->pressure / 1000;
-		val->val2 = (data->pressure % 1000) * 10000;
+		val->val2 = (data->pressure % 1000) * 1000;
 	} else {
 		rc = -ENOTSUP;
 	}
--- src/main.c
+++ src/main.c
@@ -16,8 +16,10 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 static void show_initScreen(void);
 static void show_temperature(void);
 
-static struct sensor_value temperature;
+static struct sensor_value temperature1;
+static struct sensor_value temperature2;
 static struct sensor_value humidity;
+static struct sensor_value pressure;
 
 static inline int abs(int value)
 {
@@ -27,6 +29,7 @@ static inline int abs(int value)
 void main(void)
 {
 	const struct device *rh_sens = device_get_binding("SHT2X");
+	const struct device *pr_sens = device_get_binding("MS5637");
 
 	if (rh_sens == NULL) {
 		LOG_ERR("temperature sensor not found");
@@ -43,7 +46,7 @@ void main(void)
 				rc);
 		}
 		rc = sensor_channel_get(rh_sens, SENSOR_CHAN_AMBIENT_TEMP,
-					&temperature);
+					&temperature1);
 		if (rc != 0) {
 			LOG_ERR("Getting temperature from SHT21 failed with eror %d",
 				rc);
@@ -54,6 +57,21 @@ void main(void)
 			LOG_ERR("Getting humidity from SHT21 failed with error %d",
 				rc);
 		}
+		rc = sensor_sample_fetch(pr_sens);
+		if(rc != 0) {
+			LOG_ERR("Fetching data from MS5637 failed with error %d", rc);
+		}
+		rc = sensor_channel_get(pr_sens, SENSOR_CHAN_AMBIENT_TEMP, &temperature2);
+		if (rc != 0) {
+			LOG_ERR("Getting temperature from MS5637 failed with eror %d",
+				rc);
+		}
+		rc = sensor_channel_get(pr_sens, SENSOR_CHAN_PRESS, &pressure);
+		if (rc != 0) {
+			LOG_ERR("Getting temperature from SHT21 failed with eror %d",
+				rc);
+		}
+
 		display_command(show_temperature);
 		LOG_DBG("Cycle finished");
 		k_sleep(K_SECONDS(1));
@@ -64,22 +82,39 @@ static void show_initScreen(void)
 {
 	lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);
 	lv_label_set_text(label, "Morgenmuffel");
-	lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
 	//lv_obj_set_auto_realign(label, true);
 }
 
 static void show_temperature(void)
 {
-	static lv_obj_t *label;
-	char text[] = "Nix zu sehen!";
+	static lv_obj_t *label1;
+	static lv_obj_t *label2;
+	static lv_obj_t *label3;
+	char text[] = "Hier gibt es nix zu sehen!";
 
-	if (label == NULL) {
-		label = lv_label_create(lv_scr_act(), NULL);
+	if (label1 == NULL) {
+		label1 = lv_label_create(lv_scr_act(), NULL);
+	}
+	if (label2 == NULL) {
+		label2 = lv_label_create(lv_scr_act(), NULL);
+	}
+	if (label3 == NULL) {
+		label3 = lv_label_create(lv_scr_act(), NULL);
 	}
 	snprintf(text, sizeof(text), "%s%d.%dC %d.%d%%",
-		 temperature.val1 < 0 || temperature.val2 < 0 ? "-" : "",
-		 abs(temperature.val1), abs(temperature.val2) / 10000,
-		 abs(humidity.val1), abs(humidity.val2) / 10000);
-	lv_label_set_text(label, text);
-	lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 12);
+		 temperature1.val1 < 0 || temperature1.val2 < 0 ? "-" : "",
+		 abs(temperature1.val1), abs(temperature1.val2) / 10000,
+		 humidity.val1, humidity.val2 / 10000);
+	lv_label_set_text(label1, text);
+	lv_obj_align(label1, NULL, LV_ALIGN_IN_TOP_MID, 0, 12);
+	snprintf(text, sizeof(text), "%s%d.%dC",
+		 temperature2.val1 < 0 || temperature2.val2 < 0 ? "-" : "",
+		 abs(temperature2.val1), abs(temperature2.val2) / 10000);
+	lv_label_set_text(label2, text);
+	lv_obj_align(label2, NULL, LV_ALIGN_IN_TOP_MID, 0, 24);
+	snprintf(text, sizeof(text), "%d.%dKpa",
+		 pressure.val1, pressure.val2 / 10000);
+	lv_label_set_text(label3, text);
+	lv_obj_align(label3, NULL, LV_ALIGN_IN_TOP_MID, 0, 36);
 }
