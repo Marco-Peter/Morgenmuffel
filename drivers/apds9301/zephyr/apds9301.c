6a373e34830877d7bb204f08a56c905c30f202f1
Marco Peter <marco@peter-net.ch>
2020-10-18 14:22:14 +0200

    Wrote initialisation

--- drivers/apds9301/zephyr/apds9301.c
+++ drivers/apds9301/zephyr/apds9301.c
@@ -13,19 +13,58 @@
 
 LOG_MODULE_REGISTER(apds9301, LOG_LEVEL_INF);
 
-#define APDS9301_REGISTER_ADDR_CONTROL 0x0
-#define APDS9301_REGISTER_ADDR_TIMING 0x1
-#define APDS9301_REGISTER_ADDR_THRESHLOW_LSB 0x2
-#define APDS9301_REGISTER_ADDR_THRESHLOW_MSB 0x3
-#define APDS9301_REGISTER_ADDR_THRESHHIGH_LSB 0x4
-#define APDS9301_REGISTER_ADDR_THRESHHIGH_MSB 0x5
-#define APDS9301_REGISTER_ADDR_INTERRUPT 0x6
-#define APDS9301_REGISTER_ADDR_CRC 0x8
-#define APDS9301_REGISTER_ADDR_ID 0xA
-#define APDS9301_REGISTER_ADDR_DATA0_LSB 0xC
-#define APDS9301_REGISTER_ADDR_DATA0_MSB 0xD
-#define APDS9301_REGISTER_ADDR_DATA1_LSB 0xE
-#define APDS9301_REGISTER_ADDR_DATA1_MSB 0xF
+#define REGISTER_CONTROL 0x0U
+#define REGISTER_TIMING 0x1U
+#define REGISTER_THRESHLOW_LSB 0x2U
+#define REGISTER_THRESHLOW_MSB 0x3U
+#define REGISTER_THRESHHIGH_LSB 0x4U
+#define REGISTER_THRESHHIGH_MSB 0x5U
+#define REGISTER_INTERRUPT 0x6U
+#define REGISTER_ID 0xAU
+#define REGISTER_DATA0_LSB 0xCU
+#define REGISTER_DATA0_MSB 0xDU
+#define REGISTER_DATA1_LSB 0xEU
+#define REGISTER_DATA1_MSB 0xFU
+
+#define MASK_COMMAND_CMD 0x80U
+#define OFFS_COMMAND_CMD 7
+#define MASK_COMMAND_CLEAR 0x40U
+#define OFFS_COMMAND_CLEAR 6
+#define MASK_COMMAND_WORD 0x20U
+#define OFFS_COMMAND_WORD 5
+#define MASK_COMMAND_ADDRESS 0x0FU
+#define OFFS_COMMAND_ADDRESS 0
+
+#define MASK_CONTROL_POWER 0x03U
+#define OFFS_CONTROL_POWER 0
+#define CONTROL_POWER_ON 0x03U
+#define CONTROL_POWER_OFF 0x00U
+
+#define MASK_TIMING_GAIN 0x10U
+#define OFFS_TIMING_GAIN 4
+#define MASK_TIMING_MANUAL 0x08U
+#define OFFS_TIMING_MANUAL 3
+#define MASK_TIMING_INTEG 0x03U
+#define OFFS_TIMING_INTEG 0
+#define TIMING_INTEG_MS_13_7 0x00U
+#define TIMING_INTEG_SCALE_13_7 (1U / 29U)
+#define TIMING_INTEG_MS_101 0x01U
+#define TIMING_INTEG_SCALE_101 (1U / 4U)
+#define TIMING_INTEG_MS_402 0x20U
+#define TIMING_INTEG_SCALE_402 1U
+
+#define MASK_INTERRUPT_LEVEL 0x10U
+#define OFFS_INTERRUPT_LEVEL 4
+#define MASK_INTERRUPT_PERSIST 0x0FU
+#define OFFS_INTERRUPT_PERSIST 0
+#define INTERRUPT_EVERY_CYCLE 0U
+#define INTERRUPT_OUT_OF_RANGE(_x) ((_x)&MASK_INTERRUPT_PERSIST)
+
+#define MASK_ID_PARTNUMBER 0xF0U
+#define OFFS_ID_PARTNUMBER 0x4
+#define MASK_ID_REVNO 0x0FU
+#define OFFS_ID_REVNO 0
+#define ID_PARTNUMBER ((0x5U << OFFS_ID_PARTNUMBER) & MASK_ID_PARTNUMBER)
 
 struct apds9301_config {
 	char *i2c_bus_label;
@@ -57,21 +96,56 @@ static inline const uint16_t i2c_address(const struct device *dev)
 	return config->i2c_addr;
 }
 
-static inline int send_command(const struct device *dev, uint8_t cmd)
+static inline uint8_t init_cmd(uint8_t addr, bool word, bool clear_irq)
 {
-	return i2c_write(i2c_device(dev), &cmd, sizeof(cmd), i2c_address(dev));
+	uint8_t cmd;
+
+	cmd = MASK_COMMAND_CMD | addr;
+	if (clear_irq == true) {
+		cmd |= MASK_COMMAND_CLEAR;
+	}
+	if (word == true) {
+		cmd |= MASK_COMMAND_WORD;
+	}
+	return cmd;
 }
 
-static inline int read_data(const struct device *dev, uint8_t *data,
-			    uint32_t len)
+static inline uint8_t init_len(bool word)
 {
-	return i2c_read(i2c_device(dev), data, len, i2c_address(dev));
+	uint8_t len;
+
+	if (word == true) {
+		len = 2;
+	} else {
+		len = 1;
+	}
+	return len;
+}
+
+static inline int write(const struct device *dev, uint8_t addr, uint16_t data,
+			bool word, bool clear_irq)
+{
+	uint8_t cmd = init_cmd(addr, word, clear_irq);
+	uint8_t len = init_len(word);
+
+	return i2c_burst_write(i2c_device(dev), i2c_address(dev), cmd,
+			       (uint8_t *)&data, len);
+}
+
+static inline int read(const struct device *dev, uint8_t addr, uint16_t *data,
+		       bool word, bool clear_irq)
+{
+	uint8_t cmd = init_cmd(addr, word, clear_irq);
+	uint8_t len = init_len(word);
+
+	return i2c_burst_read(i2c_device(dev), i2c_address(dev), cmd,
+			      (uint8_t *)data, len);
 }
 
 static void gpio_callback(const struct device *dev, struct gpio_callback *cb,
 			  uint32_t pins)
 {
-	struct apds9301_data *data = dev->data;
+	//struct apds9301_data *data = dev->data;
 }
 
 static int init(const struct device *dev)
@@ -79,6 +153,7 @@ static int init(const struct device *dev)
 	const struct apds9301_config *config = dev->config;
 	struct apds9301_data *data = dev->data;
 	int rc = 0;
+	uint16_t reg_data;
 
 	data->i2c = device_get_binding(config->i2c_bus_label);
 	if (data->i2c == NULL) {
@@ -106,11 +181,24 @@ static int init(const struct device *dev)
 				dev->name, rc);
 		}
 	}
+	rc = read(dev, REGISTER_ID, &reg_data, false, false);
+	if (rc != 0) {
+		LOG_ERR("%s: failed to read ID register with rc %d", dev->name,
+			rc);
+	}
+	if ((reg_data & MASK_ID_PARTNUMBER) != ID_PARTNUMBER) {
+		LOG_ERR("%s: wrong part number", dev->name);
+	}
+	rc = write(dev, REGISTER_CONTROL, CONTROL_POWER_ON, false, false);
+	if (rc != 0) {
+		LOG_ERR("%s: failed to enable the sensor", dev->name);
+	}
 	return rc;
 }
 
 static int sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
+	
 	return 0;
 }
 
