b191ed7e08e1aaca7a1c2960348184032da55ab0
Marco Peter <marco@peter-net.ch>
2020-10-30 17:33:47 +0100

    Implemented uart forwarding

---
+++ src/usb_device.c
@@ -0,0 +1,120 @@
+#include "usb_device.h"
+#include <device.h>
+#include <zephyr.h>
+#include <drivers/uart.h>
+#include <usb/usb_device.h>
+#include <sys/ring_buffer.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(usb_device, LOG_LEVEL_INF);
+
+RING_BUF_DECLARE(ringbuf_to_esp, 32);
+RING_BUF_DECLARE(ringbuf_to_host, 32);
+
+static const struct device *uart_esp;
+static const struct device *usb_cdc_esp;
+
+static void usb_cdc_handler(const struct device *dev, void *user_data);
+static void uart_esp_handler(const struct device *dev, void *user_data);
+
+int usb_device_init(void)
+{
+	int rc;
+
+	uart_esp = device_get_binding("UART_2");
+	usb_cdc_esp = device_get_binding("CDC_ACM_0");
+	if (uart_esp == NULL || usb_cdc_esp == NULL) {
+		LOG_ERR("Failed to get devices");
+		return -ENODEV;
+	}
+	rc = usb_enable(NULL);
+	if (rc != 0) {
+		LOG_ERR("Failed to enable USB with rc %d", rc);
+	}
+	uart_irq_callback_user_data_set(usb_cdc_esp, usb_cdc_handler, NULL);
+	uart_irq_callback_user_data_set(uart_esp, uart_esp_handler, NULL);
+	rc = uart_line_ctrl_set(usb_cdc_esp, UART_LINE_CTRL_DCD, 1);
+	if (rc != 0) {
+		LOG_ERR("Failed to set DCD with rc %d", rc);
+	}
+	rc = uart_line_ctrl_set(usb_cdc_esp, UART_LINE_CTRL_DSR, 1);
+	if (rc != 0) {
+		LOG_ERR("Failed to set DSR with rc %d", rc);
+	}
+	uart_irq_rx_enable(uart_esp);
+	uart_irq_rx_enable(usb_cdc_esp);
+}
+
+static int copy_to_buffer(const struct device *dev, struct ring_buf *ringbuf,
+			  uint32_t available)
+{
+	int rc;
+	uint32_t ready;
+	int used;
+	uint8_t *data;
+
+	ready = ring_buf_put_claim(ringbuf, &data, available);
+	used = uart_fifo_read(dev, data, ready);
+	rc = ring_buf_put_finish(ringbuf, used);
+	if (rc != 0) {
+		LOG_ERR("%s: invalid argument on ring_buf_put_finish!",
+			dev->name);
+	}
+	return used;
+}
+
+static void uart_handler(const struct device *dev, const struct device *other,
+			 struct ring_buf *out_buf, struct ring_buf *in_buf)
+{
+	while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
+		if (uart_irq_rx_ready(dev)) {
+			uint32_t available;
+			int used;
+
+			available = ring_buf_space_get(out_buf);
+			used = copy_to_buffer(dev, out_buf, available);
+			available -= used;
+			if (available > used) {
+				used = copy_to_buffer(dev, out_buf, available);
+				if (used == available) {
+					LOG_ERR("%s: out_buf is full!",
+						dev->name);
+				}
+			}
+			uart_irq_tx_enable(other);
+		}
+
+		if (uart_irq_tx_ready(dev)) {
+			uint32_t available;
+			int used;
+			int rc;
+			uint8_t *data;
+
+			available =
+				ring_buf_get_claim(in_buf, &data, UINT32_MAX);
+			if (available == 0U) {
+				uart_irq_tx_disable(dev);
+				continue;
+			}
+			used = uart_fifo_fill(dev, data, available);
+			rc = ring_buf_get_finish(in_buf, used);
+			if (rc != 0) {
+				LOG_ERR("invalid argument on ring_buf_get_finish!");
+			}
+		}
+	}
+}
+
+static void usb_cdc_handler(const struct device *dev, void *user_data)
+{
+	ARG_UNUSED(user_data);
+
+	uart_handler(dev, uart_esp, &ringbuf_to_esp, &ringbuf_to_host);
+}
+
+static void uart_esp_handler(const struct device *dev, void *user_data)
+{
+	ARG_UNUSED(user_data);
+
+	uart_handler(dev, usb_cdc_esp, &ringbuf_to_host, &ringbuf_to_esp);
+}
---
+++ src/usb_device.h
@@ -0,0 +1,6 @@
+#ifndef USB_DEVICE_H_
+#define USB_DEVICE_H_
+
+int usb_device_init(void);
+
+#endif /* USB_DEVICE_H_ */
